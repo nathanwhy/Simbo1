3da9a4281a9260c04eacc23e168123619815454c
nathanwhy <nathanw@126.com>
2014-06-29 14:44:23 +0800

    navigationController

---
+++ Simbo1/Main/NaviController.m
@@ -0,0 +1,43 @@
+//
+//  NaviController.m
+//  Simbo1
+//
+//  Created by NPHD on 14-5-12.
+//  Copyright (c) 2014年 Nathan Wu. All rights reserved.
+//
+
+#import "NaviController.h"
+
+@interface NaviController ()
+
+@end
+
+@implementation NaviController
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+//	UIBarButtonItem *barItem = [UIBarButtonItem appearance];
+    // 修改item的背景图片
+//    [barItem setBackgroundImage:[UIImage imageNamed:@"navigationbar_button_background.png"] forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+//    [barItem setBackgroundImage:[UIImage imageNamed:@"navigationbar_button_background_pushed.png"] forState:UIControlStateHighlighted barMetrics:UIBarMetricsDefault];
+    
+    // 修改item上面的文字样式
+//    NSDictionary *dict = @{
+//                           NSForegroundColorAttributeName : [UIColor orangeColor]
+//                           };
+//    [barItem setTitleTextAttributes:dict forState:UIControlStateNormal];
+//    [barItem setTitleTextAttributes:dict forState:UIControlStateHighlighted];
+//    [barItem setTintColor:kNaviBarTintColor];
+    
+}
+
+
+
+
+
+
+@end
