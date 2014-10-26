b24eb8874b15f566f84d514b8654da6d1b5a5adb
nathanwhy <nathanw@126.com>
2014-10-26 22:09:36 +0800

    change some content

---
+++ Simbo1/SendMessage/PostViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostViewController.h
+//  Simbo1
+//
+//  Created by nathan on 14-10-26.
+//  Copyright (c) 2014年 Nathan Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostViewController : UIViewController
+
+@end
---
+++ Simbo1/SendMessage/PostViewController.m
@@ -0,0 +1,37 @@
+//
+//  PostViewController.m
+//  Simbo1
+//
+//  Created by nathan on 14-10-26.
+//  Copyright (c) 2014年 Nathan Wu. All rights reserved.
+//
+
+#import "PostViewController.h"
+
+@interface PostViewController ()
+
+@end
+
+@implementation PostViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
